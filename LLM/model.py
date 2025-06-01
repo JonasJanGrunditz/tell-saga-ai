bc2700bcc819971fa8c9ad16acdd8ca57e5cec34
JonasJanGrunditz <123014561+JonasJanGrunditz@users.noreply.github.com>
2025-06-01 12:55:23 +0200

    done (#2)
    Co-authored-by: helloworld <jonasjonas1@hotmail.com>

--- LLM/model.py
+++ LLM/model.py
@@ -1,11 +1,25 @@
 
 from openai import OpenAI
-from LLM.prompts import SYSTEM_PROMPT, USER_PROMPT_TEMPLATE
-from LLM.structured_output import StoryGenerated
+from LLM.prompts import SYSTEM_PROMPT_CHAT, SYSTEM_PROMPT_SUGGESTIONS, USER_PROMPT_TEMPLATE_CHAT, USER_PROMPT_TEMPLATE_SUGGESTIONS
+from LLM.structured_output import StoryGenerated, SuggestionsGenerated
+from typing import Literal
 
 
-
-def call_openai(user_input: str, client: OpenAI) -> dict:
+def call_openai(
+    user_input: str,
+    client: OpenAI,
+    functionality: Literal['chat', 'suggestions']
+) -> dict:
+    if functionality == 'chat':
+        SYSTEM_PROMPT = SYSTEM_PROMPT_CHAT
+        USER_PROMPT_TEMPLATE = USER_PROMPT_TEMPLATE_CHAT
+        text_format_standrd = StoryGenerated
+    elif functionality == 'suggestions':
+        SYSTEM_PROMPT = SYSTEM_PROMPT_SUGGESTIONS
+        USER_PROMPT_TEMPLATE = USER_PROMPT_TEMPLATE_SUGGESTIONS
+        text_format_standrd = SuggestionsGenerated
+    else:
+        raise ValueError("Invalid functionality specified. Use 'chat' or 'suggestions'.")
     response = client.responses.parse(
         model="gpt-4o-2024-08-06",
         input=[
@@ -15,7 +29,7 @@ def call_openai(user_input: str, client: OpenAI) -> dict:
                 "content": USER_PROMPT_TEMPLATE.format(user_input=user_input),
             },
         ],
-    text_format=StoryGenerated,
+    text_format=text_format_standrd,
 
     )
     return response.output_parsed
